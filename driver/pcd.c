52d3496609e81b894a990f5329a67f2b078934db
sawan roy <sawan.roy25@gmail.com>
2024-02-20 21:39:43 +0530

    added the allocchrdev cdev access

--- driver/pcd.c
+++ driver/pcd.c
@@ -52,19 +52,19 @@ struct pcd_drv_private_data pcdrv_data =
             .buffer = device_buf_pcdev2,
             .size = MEM_SIZE_PCDEV2,
             .serial_num = "pcdev_1",
-            .perm = 0x1, /* RDONLY */
+            .perm = 0x10, /* WRONLY */
         },
         [2] = {
             .buffer = device_buf_pcdev3,
             .size = MEM_SIZE_PCDEV3,
             .serial_num = "pcdev_1",
-            .perm = 0x1, /* RDONLY */
+            .perm = 0x11, /* RDWR */
         },        
         [3] = {
             .buffer = device_buf_pcdev4,
             .size = MEM_SIZE_PCDEV4,
             .serial_num = "pcdev_1",
-            .perm = 0x1, /* RDONLY */
+            .perm = 0x11, /* RDWR */
         }
 
     }
@@ -72,38 +72,38 @@ struct pcd_drv_private_data pcdrv_data =
 
 
 loff_t pcd_lseek(struct file *flip, loff_t offset, int whence){
-     loff_t temp;
-    pr_info("llseek function called");
-    pr_info("curernt file position = %lld",flip->f_pos);
-    switch (whence)
-    {
-    case SEEK_SET:
-        if((offset > DEV_MEM_SIZE) || (offset < 0))
-            return -EINVAL; 
-        flip->f_pos = offset;
-        break;
-    case SEEK_CUR:
-        temp = flip->f_pos + offset;
-        if((temp > DEV_MEM_SIZE) || (temp < 0))
-            return -EINVAL;
-        flip->f_pos += offset;
-        break;
-    case SEEK_END:
-        temp = DEV_MEM_SIZE + offset;
-        if((temp > DEV_MEM_SIZE)||(temp < 0))
-            return -EINVAL;
-        flip->f_pos = DEV_MEM_SIZE+offset;
-        break;    
-    default:
-        pr_info("pcd_llseek error ");
-        return -EINVAL;
-        break;
-    }
+    //  loff_t temp;
+    // pr_info("llseek function called");
+    // pr_info("curernt file position = %lld",flip->f_pos);
+    // switch (whence)
+    // {
+    // case SEEK_SET:
+    //     if((offset > DEV_MEM_SIZE) || (offset < 0))
+    //         return -EINVAL; 
+    //     flip->f_pos = offset;
+    //     break;
+    // case SEEK_CUR:
+    //     temp = flip->f_pos + offset;
+    //     if((temp > DEV_MEM_SIZE) || (temp < 0))
+    //         return -EINVAL;
+    //     flip->f_pos += offset;
+    //     break;
+    // case SEEK_END:
+    //     temp = DEV_MEM_SIZE + offset;
+    //     if((temp > DEV_MEM_SIZE)||(temp < 0))
+    //         return -EINVAL;
+    //     flip->f_pos = DEV_MEM_SIZE+offset;
+    //     break;    
+    // default:
+    //     pr_info("pcd_llseek error ");
+    //     return -EINVAL;
+    //     break;
+    // }
     pr_info("updated file position = %lld",flip->f_pos);
     return 0;
 }
 ssize_t pcd_read(struct file *flip, char __user *buff, size_t count, loff_t *f_pos){
-
+    #if 0
     pr_info("read requerst for the %zu bytes \n",count);
     pr_info("Before reading current file position = %lld",*f_pos);
     /* Adust the count*/
@@ -120,10 +120,11 @@ ssize_t pcd_read(struct file *flip, char __user *buff, size_t count, loff_t *f_p
     *f_pos +=count;
     pr_info("NUmber of bytes successfully read = %zu \n",count);
     pr_info("Updated file position = %lld",*f_pos);
+    #endif
     return 0;
 }
 ssize_t pcd_write(struct file *flip, const char __user *buff, size_t count, loff_t *f_pos){
-
+#if 0
     pr_info("Write requerst for the %zu bytes \n",count);
     pr_info("Before Writing current file position = %lld",*f_pos);
     /* Adust the count*/
@@ -144,6 +145,7 @@ ssize_t pcd_write(struct file *flip, const char __user *buff, size_t count, loff
     *f_pos +=count;
     pr_info("NUmber of bytes successfully write = %zu \n",count);
     pr_info("Updated file position = %lld",*f_pos);
+    #endif
     return 0;
 }
 int pcd_open(struct inode *pinode, struct file *flip){
@@ -171,46 +173,53 @@ struct file_operations pcd_fops =
 static int __init pcd_driver_init(void)
 {
     int ret;
-    ret = alloc_chrdev_region(pcd_drv_private_data.d, 0, NO_OF_DEVICES, "pcd_driver");
+    int i;
+    ret = alloc_chrdev_region(&pcdrv_data.device_num, 0, NO_OF_DEVICES, "pcd_driver");
     if(ret < 0)
     {   
         pr_err("error while alloc chrdev");
         goto out;
     }
-    pr_info(" driver <MAJOR> : <MINOR> %d : %d",MAJOR(device_num),MINOR(device_num));
-    cdev_init(&pcd_cdev,&pcd_fops);
-    pcd_cdev.owner =  THIS_MODULE;    
 
-    ret = cdev_add(&pcd_cdev,device_num,1);
+    pcdrv_data.class_pcd = class_create("pcd_class");
+    if(IS_ERR(pcdrv_data.class_pcd))
+    {
+        pr_err("Class creation failed");
+        ret = PTR_ERR(pcdrv_data.class_pcd);
+        goto cdev_del;
+    }
+
+    for(i=0;i<NO_OF_DEVICES;i++){
+    pr_info(" driver <MAJOR> : <MINOR> %d : %d",MAJOR(pcdrv_data.device_num +i),MINOR(pcdrv_data.device_num+i));
+    
+    /*cdev init */
+    cdev_init(&pcdrv_data.pcdev_data[i].cdev,&pcd_fops);
+    pcdrv_data.pcdev_data[i].cdev.owner =  THIS_MODULE;    
+    ret = cdev_add(&pcdrv_data.pcdev_data[i].cdev,pcdrv_data.device_num,1);
     if(ret < 0)
     {
         pr_err("error while cdev add");
         goto chrdev_unregister;
     }
-    class_pcd = class_create(THIS_MODULE,"pcd_class");
-    if(IS_ERR(class_pcd))
-    {
-        pr_err("Class creation failed");
-        ret = PTR_ERR(class_pcd);
-        goto cdev_del;
-    }
-    device_pcd = device_create(class_pcd,NULL,1,NULL,"pcd");
-    if(IS_ERR(device_pcd))
+
+    pcdrv_data.device_pcd = device_create(pcdrv_data.class_pcd,NULL,pcdrv_data.device_num,NULL,"pcd-%d",i);
+    if(IS_ERR(pcdrv_data.device_pcd))
     {
         pr_err("device creation failed");
-        ret = PTR_ERR(device_pcd);
+        ret = PTR_ERR(pcdrv_data.device_pcd);
         goto class_distroy;
     }
-
+    }
     pr_info("init driver success");
 
     return 0;
+
 class_distroy:
-    class_destroy(class_pcd);
+    class_destroy(pcdrv_data.class_pcd);
 cdev_del:
-    cdev_del(&pcd_cdev);
+    cdev_del(&pcdrv_data.pcdev_data[i].cdev);
 chrdev_unregister:
-    unregister_chrdev_region(device_num,1);
+    unregister_chrdev_region(pcdrv_data.device_num,NO_OF_DEVICES);
 out:
     return ret;
 
@@ -218,12 +227,14 @@ out:
 
 static void __exit pcd_driver_exit(void)
 {
+    #if 0
     device_destroy(class_pcd,device_num);
     class_destroy(class_pcd);
-    cdev_del(&pcd_cdev);
+    cdev_del(&pcdev_data);
     unregister_chrdev_region(device_num,1);
 
     pr_info("module exited success");
+    #endif
 }
 
 module_init(pcd_driver_init);
